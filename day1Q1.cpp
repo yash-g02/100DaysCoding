e9c01aac3b962a0486b74a9928fb9f52ab22f128
Yash <YASH.21972@stu.upes.ac.in>
2025-08-26 16:23:19 +0530

    Add files via upload

---
+++ day1Q1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a ,b;
+	printf("enter the first number : ");
+	scanf("%d" ,&a);
+	printf("enter the second number : ");
+	scanf("%d" ,&b);
+	printf("sum of two number is : "  "%d",a+b); 
+	return 0;
+}
