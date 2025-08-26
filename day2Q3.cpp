4e82624d5a7b0587db16f9554336f740c3b99efd
Yash <YASH.21972@stu.upes.ac.in>
2025-08-26 18:32:39 +0530

    Add files via upload

---
+++ day2Q3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int p ,a ,l ,b;
+	printf("enter the length : ");
+	scanf("%d" ,&l);
+	
+	printf("enter th breath : ");
+	scanf("%d" ,&b);
+	
+	printf("perimeter of rectangle" "%d \n",2*(l+b));
+	printf("area of rectangle" "%d \n ",l*b);
+	return 0;
+}
