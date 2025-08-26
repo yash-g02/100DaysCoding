7cdce241845020bf091f78b74a409677a4fdf45e
Yash <YASH.21972@stu.upes.ac.in>
2025-08-26 17:07:22 +0530

    Add files via upload

---
+++ day1Q2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	float a ,b;
+	printf("enter the first number : ");
+	scanf("%f" ,&a);
+	
+	printf("enter the second number : ");
+	scanf("%f" ,&b);
+	
+	printf("sum of two numbers : " "%f \n" ,a+b);
+	printf("multiplication of two numbers : " "%f \n" ,a*b);
+	printf("sub of two numbers : " "%f \n",a-b);
+	printf("division of two numbers : " "%f \n" ,a/b);
+	
+	return 0;
+}
