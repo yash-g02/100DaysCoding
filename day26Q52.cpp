ed9c34ba19eff0330c4cdb1c28aa14ba2cef3339
Yash <YASH.21972@stu.upes.ac.in>
2025-09-15 21:53:53 +0530

    Add files via upload

---
+++ day26Q52.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+ {
+    int i;
+    printf("*\n\n");
+    for(i = 1; i <= 2; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+    for(i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+    for(i = 1; i <= 5; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+    for(i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+    printf("*\n");
+
+    return 0;
+}
+
