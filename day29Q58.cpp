f45638d82d586cb9a1cc63b1e547bfd922db7984
Yash <YASH.21972@stu.upes.ac.in>
2025-09-19 23:29:52 +0530

    Add files via upload

---
+++ day29Q58.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
+
